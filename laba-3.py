0b04225c38ef91963dd406392f573e568103e071
art4iba2 <99066833+art4iba2@users.noreply.github.com>
2022-03-22 14:54:28 +0400

    Update laba-3.py

--- laba-3.py
+++ laba-3.py
@@ -23,7 +23,7 @@ try:
                     quantity += 0
                     result_block.append(block)
 
-                elif block == '.' or block == ','  or block == '-': #если число дробное или отрицательное заносим в массив
+                elif block == '.' or block == ',' :                 #если число дробное заносим в массив
                     quantity += 0
                     result_block.append(block)
 
