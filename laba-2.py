f4e22da35660804c9ccf907f8cfde202da7419f1
art4iba2 <99066833+art4iba2@users.noreply.github.com>
2022-03-22 10:40:00 +0400

    Update laba-2.py

--- laba-2.py
+++ laba-2.py
@@ -1,7 +1,36 @@
-import re
-file = open ("nums.txt" , 'r')
-text = file.read()
-re_text = re.sub(r'[^\w\s]','', text)
-num = len(re_text.split())
-print ("последовательность чисел : " , re_text )
-print ("количество чисел в последовательности :" , num)
+'''Написать программу, которая читая последовательность чисел из файла, 
+выводит на экран используемые цифры и их количество'''
+
+quantity = 0                                                        #счетчик количества чисел
+buffer = 1                                                          #размер буфера чтения
+result_block = []                                                   #массив для вывода числа
+
+try:
+    with open("nums1.txt", "r") as file:                            #открывается файл
+        block = file.read(buffer)                                   #читается первый блок
+
+        while block:                                                #пока файл не пустой ищем цифры
+            while (block < '0' or block > '9') and block:
+                block = file.read(buffer)                           #читатся очередной блок
+
+            while (block >= '0' and block <= '9' or block == '.' or block == ',') and block: #обрабатываются цифры
+
+                if block.isnumeric() == True :                      #если символ число, то кол-во не увеличиваем и заносим в массив для вывода
+                    quantity += 0
+                    result_block.append(block)
+
+                elif block == '.' or block == ','  or block == '-': #если число дробное или отрицательное заносим в массив
+                    quantity += 0
+                    result_block.append(block)
+
+                block = file.read(buffer)
+                if block.isnumeric() == False and block != '.' and block != ',' and block != '-'  : #если символ не цифра и не '.' ',' '-'  то кол-во чисел увеличивваем на 1
+                  quantity += 1
+            print(result_block)                                     #выводится прочтенное число
+            result_block = []                                       #очищается массив для следующего числа
+
+
+
+        print(quantity)                                             #выводится кол-во чисел
+except FileNotFoundError:                                           #исключаем ошибку 'файл не найден' для понимания ( почему программа не работает)
+  print ("\nФайл в директории проекта не обнаружен.\nДобавьте файл в директорию или переименуйте существующий *.txt файл.")
