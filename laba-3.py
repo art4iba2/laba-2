2f98e8f1ec1058e1df5d832173b8db6592327074
art4iba2 <99066833+art4iba2@users.noreply.github.com>
2022-04-07 16:31:23 +0400

    Update laba-3.py

--- laba-3.py
+++ laba-3.py
@@ -21,25 +21,16 @@ try:
                 block = file.read(buffer)                           #читатся очередной блок
 
 
-            while (block >= '0' and block <= '9' or block == '.' or block == ',' ) and block: #обрабатываются цифры
+            while (block >= '0' and block <= '9'  ) and block: #обрабатываются цифры
 
                 if block.isnumeric() == True :                      #если символ число, то кол-во не увеличиваем и заносим в массив для вывода
                     result_block[int(block)] += 1
 
                 block = file.read(buffer)
+        for i in range (10) :
+            print("Количество цифр", i ," - ", result_block[i])
 
-        print("Количество цифр 0 - ", result_block[0])
-        print("Количество цифр 1 - ", result_block[1])
-        print("Количество цифр 2 - ", result_block[2])
-        print("Количество цифр 3 - ", result_block[3])
-        print("Количество цифр 4 - ", result_block[4])
-        print("Количество цифр 5 - ", result_block[5])
-        print("Количество цифр 6 - ", result_block[6])
-        print("Количество цифр 7 - ", result_block[7])
-        print("Количество цифр 8 - ", result_block[8])
-        print("Количество цифр 9 - ", result_block[9])
         result_block = []
 except FileNotFoundError:                                          #исключаем ошибку 'файл не найден' для понимания ( почему программа не работает)
   print ("\nФайл в директории проекта не обнаружен.\nДобавьте файл в директорию или переименуйте существующий *.txt файл.")
 print("Время работы программы:  ", time.process_time(), "seconds") #выводится время работы программы
-
